630e0cc10270923ada2100fbb6a08de521377dbf
Prasanna Wagh <prasannawagh146@gmail.com>
2024-06-23 00:55:48 +0530

    Init app

---
+++ my-app/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: 'app',
+    component : AppComponent
+  },
+  {
+    path: '',
+    redirectTo: 'index.html',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
